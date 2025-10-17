887be453a55e5805b1de7341d4a8bfe1bc19e334
dosacha <dosacha@naver.com>
2025-10-17 22:51:14 +0900

    뷰 엔진 등록하고 레이아웃 만들기

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require("dotenv").config(); // env에 있는 변수 가져오기
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 const port = process.env.PORT || 3000; // .env에 PORT가 없으면 3000번 PROT 사용
 
+// 레이아웃과 뷰 엔진 설정
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 // 루트로 접속하면 routes/main.js 의 라우트 사용
 app.use("/", require("./routes/main"));
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const mainLayout = "../views/layouts/main.ejs";
 
-router.get("/", (req, res) => {
-    res.send("Hello World!");
+router.get(["/", "home"], (req, res) => {
+    const locals = {
+        title: "Home",
+    };
+
+    // index.ejs 를 렌더링하는데 mainLayout 레이아웃으로 감싸기
+    res.render("index", { locals, layout: mainLayout});
 });
+
+router.get("/about", (req, res) => {
+    // about.ejs 를 렌더링하는데 mainLayout 레이아웃으로 감싸기
+    res.render("about", {layout: mainLayout});
+})
 
 module.exports = router;
